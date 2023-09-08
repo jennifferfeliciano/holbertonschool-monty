b904da2a0b246c533d043525f614e710c0d3ba38
carlfrank <carl-frank7@hotmail.com>
2023-09-08 09:16:34 -0400

    fucntion 2 change again

--- functions_2.c
+++ functions_2.c
@@ -1,93 +1,123 @@
 #include "monty.h"
-
 /**
  * nop - does nothing
  * @stack: stack
  * @line_number: line from file
+ * @line: pointer to line
+ * @file: file opened
  */
-void nop(stack_t **stack, unsigned int line_number)
+void nop(stack_t **stack, unsigned int line_number, char *line, FILE *file)
 {
-    (void)stack;
-    (void)line_number;
+	(void)stack;
+	(void)line_number;
+	(void)line;
+	(void)file;
 }
-
-/**
- * swap - swaps the top two elements of the stack
- * @stack: stack
- * @line_number: line number
- */
-void swap(stack_t **stack, unsigned int line_number)
+void swap(stack_t **stack, unsigned int line_number, char *line, FILE *file)
 {
-    stack_t *head = *stack;
-    stack_t *body;
+	stack_t *head = *stack;
+	stack_t *body;
+	int head_value;
+	int body_value;
 
-    if (head == NULL || (body = head->next) == NULL)
-    {
-        fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
-        exit_with_failure();
-    }
+	if (head != NULL)
+	{
+		body = (*stack)->next;
+	}
 
-    int temp = head->n;
-    head->n = body->n;
-    body->n = temp;
+	if (head != NULL && body != NULL)
+	{
+		head_value = head->n;
+		body_value = body->n;
+		head->n = body_value;
+		body->n = head_value;
+	}
+	else
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		free(tokens[1]);
+		free(tokens[0]);
+		free(tokens);
+		free(line);
+		free(*stack);
+		fclose(file);
+		exit(EXIT_FAILURE);
+	}
 }
 
 /**
  * tokenization - extract tokens from the getline command
+ *
  * @ptr: String of the command line
  * @delim: Delimiter
+ *
  * Return: the array of tokens
+ *
+ * Description: We first allocate space for the array of tokens. 
  */
 char **tokenization(char *ptr, char *delim)
 {
-    char **tokens = NULL;
-    char *token = strtok(ptr, delim);
-    int i = 0;
+	char *token = NULL, **tokens = NULL;
+	int i = 0;
 
-    tokens = malloc(sizeof(char *) * 10);
+	tokens = malloc(sizeof(char *) * 10);
+	token = strtok(ptr, delim);
 
-    while (token)
-    {
-        tokens[i] = strdup(token);
-        i++;
-        token = strtok(NULL, delim);
-    }
+	while (token)
+	{
+		tokens[i] = malloc(sizeof(char) * strlen(token) + 1);
+		strcpy(tokens[i], token);
+		i++;
+		token = NULL;
+		token = strtok(NULL, delim);
+	}
 
-    tokens[i] = NULL;
-    return tokens;
+	tokens[i] = NULL;
+	free(token);
+	return (tokens);
 }
 
-void (*get_op_func(char **tokens))(stack_t **stack, unsigned int line_number)
+void (*get_op_func(char **tokens))(stack_t **stack, unsigned int line_number, char *line, FILE *file)
 {
-    static instruction_t in_fun[] = {
-        {"push", push},
-        {"pall", pall},
-        {"pint", pint},
-        {"add", add},
-        {"nop", nop},
-        {"pop", pop},
-        {"swap", swap},
-        {NULL, unkn_func},
-    };
+	static instruction_t in_fun[] = {
+    {"push", push},
+    {"pall", pall},
+    {"pint", pint},
+    {"add", add},
+    {"nop", nop},
+    {"pop", pop},
+    {"swap", swap},
+    {NULL, unkn_func},
+};
 
-    for (int i = 0; in_fun[i].opcode != NULL; i++)
-    {
-        if (strcmp(tokens[0], in_fun[i].opcode) == 0)
-        {
-            return in_fun[i].f;
-        }
-    }
+	int i = 0;
 
-    return in_fun[7].f; // Default to unkn_func
+	
+	while (i < 7)
+	{
+		if (strcmp(tokens[0], in_fun[i].opcode) == 0)
+		{
+			return (in_fun[i].f);
+		}
+		i++;
+	}
+	return (in_fun[i].f);
 }
-
 /**
  * unkn_func - print error message and free memory
  * @stack: stack
  * @line_number: number of line
+ * @line: pointer to line
+ * @file: opened file
  */
-void unkn_func(stack_t **stack, unsigned int line_number)
+void unkn_func(stack_t **stack, unsigned int line_number, char *line, FILE *file)
 {
-    fprintf(stderr, "L%d: unknown instruction %s\n", line_number, tokens[0]);
-    exit_with_failure();
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, tokens[0]);
+	free(tokens[1]);
+	free(tokens[0]);
+	free(tokens);
+	free(line);
+	free_listint(*stack);
+	fclose(file);
+	exit(EXIT_FAILURE);
 }
